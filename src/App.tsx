597670cd3615912c7721d723492bd4423e9a1b13
Egor Shkirya <zuhijan@yandex.ru>
2021-04-27 11:52:51 +0600

    make layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import AppHeader from "./components/AppHeader/AppHeader";
+import BurgerIngredients from "./components/BurgerIngredients/BurgerIngredients";
+import BurgerConstructor from "./components/BurgerConstructor/BurgerContstuctor";
+import { INGREDIENTS, ingredientType } from "./utils/data";
+import formatDataIngredients from "./utils/formatDataIngredients";
 
 function App() {
+  const [selectedBun, setSelectedBun] = useState<ingredientType | undefined>();
+  const [selectedIngredients, setSelectedIngredients] = useState<
+    ingredientType[]
+  >([]);
+
+  const ingredientsData = formatDataIngredients(INGREDIENTS);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+      <section className="main content-wrapper">
+        <h1 className="text m-2 text_type_main-large">Соберите бургер</h1>
+        <div className="burger-container">
+          <BurgerIngredients ingredients={ingredientsData} />
+          <BurgerConstructor
+            bun={selectedBun}
+            ingredients={[...ingredientsData.main, ...ingredientsData.sauce]}
+          />
+        </div>
+      </section>
     </div>
   );
 }
