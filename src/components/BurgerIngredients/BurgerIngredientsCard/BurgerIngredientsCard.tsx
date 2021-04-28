6830148d22e18d8205604f15f7f12b1c8a4f37fe
Egor Shkirya <zuhijan@yandex.ru>
2021-04-28 09:03:19 +0600

    add counter

--- src/components/BurgerIngredients/BurgerIngredientsCard/BurgerIngredientsCard.tsx
+++ src/components/BurgerIngredients/BurgerIngredientsCard/BurgerIngredientsCard.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons/currency-icon";
 import s from "./burgerIngredientsCard.module.scss";
+import { Counter } from "@ya.praktikum/react-developer-burger-ui-components";
 
 interface IBurgerIngredientsCard {
   img: string;
@@ -15,6 +16,7 @@ const BurgerIngredientsCard: FC<IBurgerIngredientsCard> = ({
 }) => {
   return (
     <div className={s.card}>
+      <Counter count={12} size="small" />
       <img src={img} alt={name} />
       <div className={s.priceRow}>
         <p className="m-1 text_type_digits-default">{price}</p>
