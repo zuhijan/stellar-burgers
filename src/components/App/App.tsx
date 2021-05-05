4a53e1fb8f68bf8135b932e3f594f86b4f5f03a5
Egor Shkirya <zuhijan@yandex.ru>
2021-05-05 13:15:26 +0600

    add request in useEffect

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.module.scss";
 import AppHeader from "../AppHeader/AppHeader";
 import BurgerIngredients from "../BurgerIngredients/BurgerIngredients";
 import BurgerConstructor from "../BurgerConstructor/BurgerContstuctor";
-import { INGREDIENTS, ingredientType } from "../../utils/data";
+import { ingredientType } from "../../utils/data";
 import formatDataIngredients from "../../utils/formatDataIngredients";
 import s from "./App.module.scss";
 import clsx from "clsx";
+
+const API_URL = "https://norma.nomoreparties.space/api/ingredients";
+
+export type IngredientDataType = {
+  bun: ingredientType[];
+  sauce: ingredientType[];
+  main: ingredientType[];
+};
 
 function App() {
   const [selectedBun, setSelectedBun] = useState<ingredientType | undefined>();
   const [selectedIngredients, setSelectedIngredients] = useState<
     ingredientType[]
   >([]);
+  const [ingredients, setIngredients] = useState<IngredientDataType>({
+    main: [],
+    bun: [],
+    sauce: [],
+  });
 
-  const ingredientsData = formatDataIngredients(INGREDIENTS);
+  useEffect(() => {
+    const getIngredients = async () => {
+      try {
+        const res = await fetch(API_URL);
+        const ingredients = await res.json();
+        const ingredientsData = formatDataIngredients(ingredients.data);
+        setIngredients(ingredientsData);
+      } catch (err) {
+        console.log(`### err.message`, err.message);
+      }
+    };
+    getIngredients();
+  }, []);
 
   return (
     <div className={s.App}>
@@ -24,10 +49,10 @@ function App() {
           Соберите бургер
         </h1>
         <div className={s.burgerContainer}>
-          <BurgerIngredients ingredients={ingredientsData} />
+          <BurgerIngredients ingredients={ingredients} />
           <BurgerConstructor
             bun={selectedBun}
-            ingredients={[...ingredientsData.main, ...ingredientsData.sauce]}
+            ingredients={[...ingredients.main, ...ingredients.sauce]}
           />
         </div>
       </section>
