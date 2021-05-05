084f42bd2e7d660e9b8d676b768db631b4f27e89
Egor Shkirya <zuhijan@yandex.ru>
2021-05-05 13:16:50 +0600

    add logic open modal IngredientDetails + add memo

--- src/components/BurgerIngredients/BurgerIngredientsCard/BurgerIngredientsCard.tsx
+++ src/components/BurgerIngredients/BurgerIngredientsCard/BurgerIngredientsCard.tsx
@@ -1,29 +1,35 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons/currency-icon";
 import s from "./burgerIngredientsCard.module.scss";
 import { Counter } from "@ya.praktikum/react-developer-burger-ui-components";
+import IngredientDetails from "../../IngredientDetails/IngredientDetails";
+import { ingredientType } from "../../../utils/data";
 
 interface IBurgerIngredientsCard {
-  img: string;
-  price: number;
-  name: string;
+  ingredient: ingredientType;
 }
 
-const BurgerIngredientsCard: FC<IBurgerIngredientsCard> = ({
-  img,
-  price,
-  name,
-}) => {
+const BurgerIngredientsCard: FC<IBurgerIngredientsCard> = ({ ingredient }) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className={s.card}>
-      <Counter count={12} size="small" />
-      <img src={img} alt={name} />
-      <div className={s.priceRow}>
-        <p className="m-1 text_type_digits-default">{price}</p>
-        <CurrencyIcon type="primary" />
+    <>
+      <div onClick={() => setOpen(true)} className={s.card}>
+        <Counter count={12} size="small" />
+        <img src={ingredient.image} alt={ingredient.name} />
+        <div className={s.priceRow}>
+          <p className="m-1 text_type_digits-default">{ingredient.price}</p>
+          <CurrencyIcon type="primary" />
+        </div>
+        <p className="m-1 text_type_main-default">{ingredient.name}</p>
       </div>
-      <p className="m-1 text_type_main-default">{name}</p>
-    </div>
+      {open && (
+        <IngredientDetails
+          ingredient={ingredient}
+          onClose={() => setOpen(false)}
+        />
+      )}
+    </>
   );
 };
-export default BurgerIngredientsCard;
+export default React.memo(BurgerIngredientsCard);
